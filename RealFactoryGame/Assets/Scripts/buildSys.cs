00237068ee623f7a044a26b05b4d8e652306fec9
zturtledog <71353802+zturtledog@users.noreply.github.com>
2021-07-22 10:57:32 -0700

    besma

--- RealFactoryGame/Assets/Scripts/buildSys.cs
+++ RealFactoryGame/Assets/Scripts/buildSys.cs
@@ -22,7 +22,7 @@ public class buildSys : MonoBehaviour
 
         if (Physics.Raycast(ray, out hit))
         {
-            if (hit.transform.gameObject.tag == "anchor" && Input.GetMouseButtonDown(1) && buildScript.currentItem().holdingObj && buildScript.idaho && buildScript.currentItem().ammount > 0)//check if player is holding an obj
+            if (hit.transform.gameObject.tag == "anchor" && Input.GetMouseButtonDown(0) && buildScript.currentItem().holdingObj && buildScript.idaho && buildScript.currentItem().ammount > 0)//check if player is holding an obj
             {
                 Instantiate(buildScript.currentItem().main, hit.point, Quaternion.identity);
                 buildScript.removeCurrentItem();//remove item
