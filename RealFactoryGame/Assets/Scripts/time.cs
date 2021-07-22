b61364bb6398a3bb50c0c902dd889ae78da9676c
zturtledog <71353802+zturtledog@users.noreply.github.com>
2021-07-21 21:53:23 -0700

    dayfix

--- RealFactoryGame/Assets/Scripts/time.cs
+++ RealFactoryGame/Assets/Scripts/time.cs
@@ -6,6 +6,9 @@ public class time : MonoBehaviour
 {
     public GameObject sun;
     public bool isday = true;
+    public string[] mooncycle;
+    public Quaternion currentRotation;
+    public int index;
     
     // Start is called before the first frame update
     void Start()
@@ -17,12 +20,25 @@ public class time : MonoBehaviour
     {
         sun.transform.Rotate(new Vector3(0.5f*Time.deltaTime,0,0));
 
-        if (sun.transform.rotation.eulerAngles.x < 0)
+        if (index == mooncycle.Length)
+        {
+            index = 0;
+        }
+
+        if (sun.transform.rotation.eulerAngles.x > 180 )
         {
             isday = false;
         }else
         {
             isday = !false;
+        }
+
+        if (sun.transform.rotation.eulerAngles.x >= 360)
+        {
+            currentRotation.eulerAngles = Vector3.zero;
+            sun.transform.rotation = currentRotation;
+            index++;
+            Debug.Log(mooncycle[index]);
         }
     }
 }
