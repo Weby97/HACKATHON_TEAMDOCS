8b905d2635705ba0567fc59c16bc81b53378546f
Dane <danechaky@icloud.com>
2021-07-22 14:46:57 -0400

    Some Stuff

--- RealFactoryGame/Assets/Scripts/bad_to_player.cs
+++ RealFactoryGame/Assets/Scripts/bad_to_player.cs
@@ -20,7 +20,25 @@ public class bad_to_player : MonoBehaviour
     {
         body.transform.position = new Vector3(agent.nextPosition.x,agent.transform.position.y,agent.nextPosition.z);
         agent.SetDestination(target.transform.position);
+        Quaternion rotation;
+        Vector3 targetPos = computeRotation(90, 0.05f, target.transform.position, target.transform.forward);
+
+        rotation = Quaternion.LookRotation(targetPos - transform.position);
+
+        
         agent.speed = speed;
+    }
+    Vector3 computeRotation(float degrees, float distance, Vector3 origin, Vector3 forward)
+    {
+        Quaternion rotation;
+        Vector3 direction;
+        Vector3 result;
+
+        rotation = Quaternion.AngleAxis(degrees, Vector3.up);
+        direction = rotation * forward * distance;
+        result = origin + direction;
+
+        return result;
     }
 }
 
